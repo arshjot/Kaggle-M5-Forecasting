90e6bcdb066a0ac2dfcc38d7f8893d5809d6e0fc
arshjot <arshjot@live.com>
2020-06-26 18:53:17 +0530

    Make the data loader more memory efficient

--- uncertainty_stream/data_loader/data_generator.py
+++ uncertainty_stream/data_loader/data_generator.py
@@ -38,15 +38,16 @@ class CustomDataset(data_utils.Dataset):
     y: targets (only in training phase)
     """
 
-    def __init__(self, X_prev_day_sales, X_enc_only_feats, X_enc_dec_feats, X_calendar, X_prev_day_sales_dec,
-                 norm_factor, Y=None, rmsse_denominator=None, wrmsse_weights=None, window_id=None):
+    def __init__(self, X_prev_day_sales, X_enc_only_feats, X_enc_dec_feats, X_calendar, norm_factor, norm_factor_sell_p,
+                 window_time_range, Y=None, rmsse_denominator=None, wrmsse_weights=None, window_id=None):
 
         self.X_prev_day_sales = X_prev_day_sales
         self.X_enc_only_feats = X_enc_only_feats
         self.X_enc_dec_feats = X_enc_dec_feats
         self.X_calendar = X_calendar
-        self.X_prev_day_sales_dec = X_prev_day_sales_dec
         self.norm_factor = norm_factor
+        self.norm_factor_sell_p = norm_factor_sell_p
+        self.window_time_range = window_time_range
         self.window_id = window_id
 
         if Y is not None:
@@ -57,60 +58,71 @@ class CustomDataset(data_utils.Dataset):
             self.Y = None
 
     def __len__(self):
-        return self.X_prev_day_sales.shape[1]
+        return self.norm_factor.shape[0]
 
     def __getitem__(self, idx):
         if self.window_id is not None:
-            X_calendar = self.X_calendar[self.window_id[idx]]
+            time_range = self.window_time_range[self.window_id[idx]]
             scale = self.rmsse_denominator[idx - (self.window_id[idx] * 42840)]
             weight = self.wrmsse_weights[idx - (self.window_id[idx] * 42840)]
             ids_idx = idx - (self.window_id[idx] * 42840)
             window_id = self.window_id[idx]
         else:
-            X_calendar = self.X_calendar
             if self.Y is not None:
+                time_range = self.window_time_range
                 scale = self.rmsse_denominator[idx]
                 weight = self.wrmsse_weights[idx]
                 ids_idx = idx
                 window_id = 0
 
-        enc_timesteps = self.X_prev_day_sales.shape[0]
-        dec_timesteps = self.X_enc_dec_feats.shape[0] - enc_timesteps
+        # Filter data for time range of the selected window, also normalize prev_day_sales and sell_price
+        norm_factor = self.norm_factor[idx]
+        X_calendar = self.X_calendar[time_range[0]:time_range[2]]
+
+        X_prev_day_sales = self.X_prev_day_sales[time_range[0]:time_range[1], ids_idx] / norm_factor
+        X_prev_day_sales_dec = self.X_prev_day_sales[time_range[1]:time_range[2], ids_idx] / norm_factor
+        X_prev_day_sales[X_prev_day_sales < 0] = -1.0
+        X_prev_day_sales_dec[X_prev_day_sales_dec < 0] = -1.0
+
+        X_enc_dec_feats = self.X_enc_dec_feats[time_range[0]:time_range[2], ids_idx]
+        X_enc_dec_feats[:, 0] = X_enc_dec_feats[:, 0] / self.norm_factor_sell_p[idx]
+
+        Y = self.Y[ids_idx, time_range[1]:time_range[2]]
+
+        enc_timesteps = time_range[1] - time_range[0]
+        dec_timesteps = time_range[2] - time_range[0] - enc_timesteps
         num_embedding = 5
         num_cal_embedding = 2
 
         # input data for encoder
-        x_enc_dec_feats_enc = self.X_enc_dec_feats[:enc_timesteps, idx, :-num_embedding].reshape(enc_timesteps, -1)
-        # x_enc_only_feats = self.X_enc_only_feats[:, idx, :].reshape(enc_timesteps, -1)
-        x_prev_day_sales_enc = self.X_prev_day_sales[:, idx].reshape(-1, 1)
+        x_enc_dec_feats_enc = X_enc_dec_feats[:enc_timesteps, :-num_embedding].reshape(enc_timesteps, -1)
+        x_prev_day_sales_enc = X_prev_day_sales.reshape(-1, 1)
         x_calendar_enc = X_calendar[:enc_timesteps, :-num_cal_embedding]
         x_calendar_enc_emb = X_calendar[:enc_timesteps, -num_cal_embedding:].reshape(enc_timesteps, -1)
-        # x_enc = np.concatenate([x_enc_dec_feats_enc, x_calendar_enc,
-        #                         x_prev_day_sales_enc, x_enc_only_feats], axis=1)
         x_enc = np.concatenate([x_enc_dec_feats_enc, x_calendar_enc, x_prev_day_sales_enc], axis=1)
-        x_enc_emb = self.X_enc_dec_feats[:enc_timesteps, idx, -num_embedding:].reshape(enc_timesteps, -1)
+        x_enc_emb = X_enc_dec_feats[:enc_timesteps, -num_embedding:].reshape(enc_timesteps, -1)
 
         # input data for decoder
-        x_enc_dec_feats_dec = self.X_enc_dec_feats[enc_timesteps:, idx, :-num_embedding].reshape(dec_timesteps, -1)
+        x_enc_dec_feats_dec = X_enc_dec_feats[enc_timesteps:, :-num_embedding].reshape(dec_timesteps, -1)
         x_calendar_dec = X_calendar[enc_timesteps:, :-num_cal_embedding]
         x_calendar_dec_emb = X_calendar[enc_timesteps:, -num_cal_embedding:].reshape(dec_timesteps, -1)
-        x_prev_day_sales_dec = self.X_prev_day_sales_dec[:, idx].reshape(-1, 1)
+        x_prev_day_sales_dec = X_prev_day_sales_dec.reshape(-1, 1)
         x_dec = np.concatenate([x_enc_dec_feats_dec, x_calendar_dec], axis=1)
-        x_dec_emb = self.X_enc_dec_feats[enc_timesteps:, idx, -num_embedding:].reshape(dec_timesteps, -1)
+        x_dec_emb = X_enc_dec_feats[enc_timesteps:, -num_embedding:].reshape(dec_timesteps, -1)
 
         if self.Y is None:
             return [[torch.from_numpy(x_enc).float(), torch.from_numpy(x_enc_emb).long(),
                      torch.from_numpy(x_calendar_enc_emb).long(),
                      torch.from_numpy(x_dec).float(), torch.from_numpy(x_dec_emb).long(),
                      torch.from_numpy(x_calendar_dec_emb).long(),
-                     torch.from_numpy(x_prev_day_sales_dec).float()], self.norm_factor[idx]]
+                     torch.from_numpy(x_prev_day_sales_dec).float()], norm_factor]
 
         return [[torch.from_numpy(x_enc).float(), torch.from_numpy(x_enc_emb).long(),
                  torch.from_numpy(x_calendar_enc_emb).long(),
                  torch.from_numpy(x_dec).float(), torch.from_numpy(x_dec_emb).long(),
                  torch.from_numpy(x_calendar_dec_emb).long(),
                  torch.from_numpy(x_prev_day_sales_dec).float()],
-                self.Y[idx, :], torch.from_numpy(np.array(self.norm_factor[idx])).float(),
+                Y, torch.from_numpy(np.array(norm_factor)).float(),
                 ids_idx,
                 [scale, weight],
                 window_id]
@@ -135,11 +147,18 @@ class DataLoader:
         self.sell_price_l12 = self.X_enc_dec_feats[:, :, self.sell_price_i]
         sell_price_all, _, _ = get_aggregated_series(self.X_enc_dec_feats[:, :, self.sell_price_i].T, self.ids, 'mean')
         encodings_all, _ = get_aggregated_encodings(self.X_enc_dec_feats[:, :, 1:].transpose(1, 0, 2), self.ids)
-        self.X_enc_dec_feats = np.concatenate([sell_price_all[:, :, np.newaxis], encodings_all], axis=2)\
+        self.X_enc_dec_feats = np.concatenate([sell_price_all[:, :, np.newaxis], encodings_all], axis=2) \
             .transpose(1, 0, 2)
 
         self.Y_l12 = data_dict['Y']
         self.Y, _, _ = get_aggregated_series(data_dict['Y'], self.ids)
+        self.Y = self.Y
+
+        # for prev_day_sales, set value as -1 for the period the product was not actively sold
+        self.X_prev_day_sales_unsold_negative = self.X_prev_day_sales.copy()
+        for idx, first_non_zero_idx in enumerate((self.X_prev_day_sales != 0).argmax(axis=0)):
+            self.X_prev_day_sales_unsold_negative[:first_non_zero_idx, idx] = -1
+        self.X_prev_day_sales_unsold_negative = self.X_prev_day_sales_unsold_negative
 
         self.X_enc_only_feats = data_dict['X_enc_only_feats']
         self.X_calendar = data_dict['X_calendar']
@@ -154,12 +173,14 @@ class DataLoader:
         # Run a sliding window of length "window_length" and train for the next month of each window
         if self.config.sliding_window:
             window_length = self.config.window_length
-            X_prev_day_sales, X_enc_only_feats, X_enc_dec_feats, X_calendar, Y, norm_factor = [], [], [], [], [], []
-            X_prev_day_sales_dec, weights, scales = [], [], []
+            window_time_range, norm_factor, norm_factor_sell_p = [], [], []
+            weights, scales = [], []
 
             for idx, i in enumerate(range(data_start_t + window_length, horizon_end_t, 28)):
                 w_data_start_t, w_horizon_start_t = data_start_t + (idx * 28), i
                 w_horizon_end_t = w_horizon_start_t + 28
+                window_time_range.append([w_data_start_t - data_start_t, w_horizon_start_t - data_start_t,
+                                          w_horizon_end_t - data_start_t])
 
                 # calculate denominator for rmsse loss
                 absolute_movement = np.absolute(self.Y.T[:w_horizon_start_t] -
@@ -174,42 +195,29 @@ class DataLoader:
 
                 # Get weights for WRMSSE and SPL loss
                 w_weights, _ = get_weights_all_levels(self.Y_l12[:, w_horizon_start_t - 28:w_horizon_start_t],
-                                                      self.sell_price_l12[w_horizon_start_t - 28:w_horizon_start_t, :].T,
+                                                      self.sell_price_l12[w_horizon_start_t - 28:w_horizon_start_t,
+                                                      :].T,
                                                       self.ids)
                 weights.append(w_weights)
 
                 # Normalize sale features by dividing by mean of each series (as per the selected input window)
-                w_X_prev_day_sales = self.X_prev_day_sales[w_data_start_t:w_horizon_start_t].copy().astype(float)
-                w_norm_factor = np.mean(w_X_prev_day_sales, 0)
+                w_X_prev_day_sales_calc = self.X_prev_day_sales[w_data_start_t:w_horizon_start_t]
+                w_norm_factor = np.mean(w_X_prev_day_sales_calc, 0)
                 w_norm_factor[w_norm_factor == 0] = 1.
-                w_X_prev_day_sales = w_X_prev_day_sales / w_norm_factor
-                w_X_prev_day_sales_dec = self.X_prev_day_sales[w_horizon_start_t:w_horizon_end_t]\
-                                             .copy().astype(float) / w_norm_factor
-                
-                w_X_enc_dec_feats = self.X_enc_dec_feats[w_data_start_t:w_horizon_end_t]
-                w_X_sell_p = self.X_enc_dec_feats[w_data_start_t:w_horizon_start_t, :, self.sell_price_i].copy().astype(float)
+
+                w_X_sell_p = self.X_enc_dec_feats[w_data_start_t:w_horizon_start_t, :, self.sell_price_i].copy().astype(
+                    float)
                 w_norm_factor_sell_p = np.median(w_X_sell_p, 0)
                 w_norm_factor_sell_p[w_norm_factor_sell_p == 0] = 1.
-                w_X_enc_dec_feats[:, :, self.sell_price_i] = w_X_enc_dec_feats[:, :, self.sell_price_i] / w_norm_factor_sell_p
-
-                X_prev_day_sales.append(w_X_prev_day_sales)
-                X_enc_only_feats.append(self.X_enc_only_feats[w_data_start_t:w_horizon_start_t])
-                X_enc_dec_feats.append(w_X_enc_dec_feats)
-                X_calendar.append(self.X_calendar[w_data_start_t:w_horizon_end_t])
-                X_prev_day_sales_dec.append(w_X_prev_day_sales_dec)
                 norm_factor.append(w_norm_factor)
-                Y.append(self.Y[:, w_horizon_start_t:w_horizon_end_t])
+                norm_factor_sell_p.append(w_norm_factor_sell_p)
 
             self.n_windows = idx + 1
-            X_prev_day_sales = np.concatenate(X_prev_day_sales, 1)
-            X_enc_only_feats = np.concatenate(X_enc_only_feats, 1)
-            X_enc_dec_feats = np.concatenate(X_enc_dec_feats, 1)
-            X_calendar = np.stack(X_calendar, 0)
-            X_prev_day_sales_dec = np.concatenate(X_prev_day_sales_dec, 1)
-            Y = np.concatenate(Y, 0)
             scales = np.concatenate(scales, 0)
             weights = np.concatenate(weights, 0)
             norm_factor = np.concatenate(norm_factor, 0)
+            norm_factor_sell_p = np.concatenate(norm_factor_sell_p, 0)
+            window_time_range = np.array(window_time_range)
             window_id = np.arange(idx + 1).repeat(self.X_enc_dec_feats.shape[1])
 
         else:
@@ -229,31 +237,24 @@ class DataLoader:
                                                 self.ids)
 
             # Normalize sale features by dividing by mean of each series (as per the selected input window)
-            X_prev_day_sales = self.X_prev_day_sales[data_start_t:horizon_start_t].copy().astype(float)
-            norm_factor = np.mean(X_prev_day_sales, 0)
+            X_prev_day_sales_calc = self.X_prev_day_sales[data_start_t:horizon_start_t]
+            norm_factor = np.mean(X_prev_day_sales_calc, 0)
             norm_factor[norm_factor == 0] = 1.
-            X_prev_day_sales = X_prev_day_sales / norm_factor
-            X_prev_day_sales_dec = self.X_prev_day_sales[horizon_start_t:horizon_end_t]\
-                                       .copy().astype(float) / norm_factor
-            
-            X_enc_dec_feats = self.X_enc_dec_feats[data_start_t:horizon_end_t]
+
             X_sell_p = self.X_enc_dec_feats[data_start_t:horizon_start_t, :, self.sell_price_i].copy().astype(float)
             norm_factor_sell_p = np.median(X_sell_p, 0)
             norm_factor_sell_p[norm_factor_sell_p == 0] = 1.
-            X_enc_dec_feats[:, :, self.sell_price_i] = X_enc_dec_feats[:, :, self.sell_price_i] / norm_factor_sell_p
 
-            X_enc_only_feats = self.X_enc_only_feats[data_start_t:horizon_start_t]
-            X_calendar = self.X_calendar[data_start_t:horizon_end_t]
-            Y = self.Y[:, horizon_start_t:horizon_end_t]
+            window_time_range = [0, horizon_start_t - data_start_t, horizon_end_t - data_start_t]
             scales = np.array(rmsse_den)
             window_id = None
 
-        dataset = CustomDataset(X_prev_day_sales,
-                                X_enc_only_feats,
-                                X_enc_dec_feats,
-                                X_calendar, X_prev_day_sales_dec,
-                                norm_factor,
-                                Y=Y,
+        dataset = CustomDataset(self.X_prev_day_sales_unsold_negative[data_start_t:],
+                                self.X_enc_only_feats[data_start_t:],
+                                self.X_enc_dec_feats[data_start_t:],
+                                self.X_calendar[data_start_t:],
+                                norm_factor, norm_factor_sell_p, window_time_range,
+                                Y=self.Y[:, data_start_t:],
                                 rmsse_denominator=scales, wrmsse_weights=weights, window_id=window_id)
 
         return torch.utils.data.DataLoader(dataset=dataset, batch_size=self.config.batch_size, shuffle=True,
@@ -276,30 +277,27 @@ class DataLoader:
             rmsse_den.append(den)
 
         # Get weights for WRMSSE and SPL loss
-        weights, _ = get_weights_all_levels(self.Y_l12[:, horizon_start_t-28:horizon_start_t],
-                                            self.sell_price_l12[horizon_start_t-28:horizon_start_t, :].T,
+        weights, _ = get_weights_all_levels(self.Y_l12[:, horizon_start_t - 28:horizon_start_t],
+                                            self.sell_price_l12[horizon_start_t - 28:horizon_start_t, :].T,
                                             self.ids)
 
         # Normalize sale features by dividing by mean of each series (as per the selected input window)
-        X_prev_day_sales = self.X_prev_day_sales[data_start_t:horizon_start_t].copy().astype(float)
-        norm_factor = np.mean(X_prev_day_sales, 0)
+        X_prev_day_sales_calc = self.X_prev_day_sales[data_start_t:horizon_start_t]
+        norm_factor = np.mean(X_prev_day_sales_calc, 0)
         norm_factor[norm_factor == 0] = 1.
-        X_prev_day_sales = X_prev_day_sales / norm_factor
-        X_prev_day_sales_dec = self.X_prev_day_sales[horizon_start_t:horizon_end_t] \
-                                   .copy().astype(float) / norm_factor
 
-        X_enc_dec_feats = self.X_enc_dec_feats[data_start_t:horizon_end_t]
         X_sell_p = self.X_enc_dec_feats[data_start_t:horizon_start_t, :, self.sell_price_i].copy().astype(float)
         norm_factor_sell_p = np.median(X_sell_p, 0)
         norm_factor_sell_p[norm_factor_sell_p == 0] = 1.
-        X_enc_dec_feats[:, :, self.sell_price_i] = X_enc_dec_feats[:, :, self.sell_price_i] / norm_factor_sell_p
 
-        dataset = CustomDataset(X_prev_day_sales,
-                                self.X_enc_only_feats[data_start_t:horizon_start_t],
-                                X_enc_dec_feats,
-                                self.X_calendar[data_start_t:horizon_end_t], X_prev_day_sales_dec,
-                                norm_factor,
-                                Y=self.Y[:, horizon_start_t:horizon_end_t],
+        window_time_range = [0, horizon_start_t - data_start_t, horizon_end_t - data_start_t]
+
+        dataset = CustomDataset(self.X_prev_day_sales_unsold_negative[data_start_t:],
+                                self.X_enc_only_feats[data_start_t:],
+                                self.X_enc_dec_feats[data_start_t:],
+                                self.X_calendar[data_start_t:],
+                                norm_factor, norm_factor_sell_p, window_time_range,
+                                Y=self.Y[:, data_start_t:],
                                 rmsse_denominator=np.array(rmsse_den), wrmsse_weights=weights)
 
         return torch.utils.data.DataLoader(dataset=dataset, batch_size=self.config.batch_size, num_workers=3,
@@ -312,24 +310,21 @@ class DataLoader:
             horizon_end_t = self.config.test_ts['horizon_end_t']
 
         # Normalize sale features by dividing by mean of each series (as per the selected input window)
-        X_prev_day_sales = self.X_prev_day_sales[data_start_t:horizon_start_t].copy().astype(float)
-        norm_factor = np.mean(X_prev_day_sales, 0)
+        X_prev_day_sales_calc = self.X_prev_day_sales[data_start_t:horizon_start_t]
+        norm_factor = np.mean(X_prev_day_sales_calc, 0)
         norm_factor[norm_factor == 0] = 1.
-        X_prev_day_sales = X_prev_day_sales / norm_factor
-        X_prev_day_sales_dec = self.X_prev_day_sales[horizon_start_t:horizon_end_t] \
-                                   .copy().astype(float) / norm_factor
 
-        X_enc_dec_feats = self.X_enc_dec_feats[data_start_t:horizon_end_t]
         X_sell_p = self.X_enc_dec_feats[data_start_t:horizon_start_t, :, self.sell_price_i].copy().astype(float)
         norm_factor_sell_p = np.median(X_sell_p, 0)
         norm_factor_sell_p[norm_factor_sell_p == 0] = 1.
-        X_enc_dec_feats[:, :, self.sell_price_i] = X_enc_dec_feats[:, :, self.sell_price_i] / norm_factor_sell_p
-        
-        dataset = CustomDataset(X_prev_day_sales,
-                                self.X_enc_only_feats[data_start_t:horizon_start_t],
-                                X_enc_dec_feats,
-                                self.X_calendar[data_start_t:horizon_end_t], X_prev_day_sales_dec,
-                                norm_factor)
+
+        window_time_range = [0, horizon_start_t - data_start_t, horizon_end_t - data_start_t]
+
+        dataset = CustomDataset(self.X_prev_day_sales_unsold_negative[data_start_t:],
+                                self.X_enc_only_feats[data_start_t:],
+                                self.X_enc_dec_feats[data_start_t:],
+                                self.X_calendar[data_start_t:],
+                                norm_factor, norm_factor_sell_p, window_time_range)
 
         return torch.utils.data.DataLoader(dataset=dataset, batch_size=self.config.batch_size, num_workers=3,
                                            pin_memory=True)
